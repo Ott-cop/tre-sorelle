5e827c487e83d01705d495643c5f483422d96fdb
Otávio Onoue <otavio.onoue@outlook.com>
2023-08-29 04:18:20 -0300

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::*;
+use actix_files as fs;
+use std::path::*;
+
+#[actix_web::main]
+async fn main() {
+    let _ = HttpServer::new(|| {
+        App::new()
+            .service(fs::Files::new("/static", Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/static"))))
+            .service(index)
+    })
+    .bind("127.0.0.1:3001")
+    .unwrap()
+    .run()
+    .await;
+}
+
+
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body(include_str!("../static/index.html"))
+}
